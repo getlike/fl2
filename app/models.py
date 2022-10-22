f15c3aee5ae47998d9ac0defedc0ed3e7391c821
getlike <itsr.org@gmail.com>
2022-10-22 09:13:11 +0200

    add models db posts

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(120), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-
+    posts = db.relationship('Posts', backref='author', lazy='dynamic')
     def __repr__(self):
         return '<User {}>'.format(self.username)
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.Datatime, index=True, default=datetime.utcnow)
+    user_id=db.Column(db.Integer, db.ForegnKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
